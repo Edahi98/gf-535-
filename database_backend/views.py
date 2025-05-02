d40e2db84a7732d66711cc60c57b9430b6542cf5
Edahi98 <garciaedahi3822@gmail.com>
2025-05-02 14:42:54 -0600

    se agrego jwt [Correacion]

--- database_backend/views.py
+++ database_backend/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 from database_backend.models import ArticuloModel, TipoArticuloModel, NotaModel
 from database_backend.serializers import ArticuloSerializer, TipoArticuloSerializer, NotaSerializer
 
 
 class ArticuloViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
     serializer_class = ArticuloSerializer
     queryset = ArticuloModel.objects.all()
 
 class TipoArticuloViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
     serializer_class = TipoArticuloSerializer
     queryset = TipoArticuloModel.objects.all()
 
 class NotaViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
     serializer_class = NotaSerializer
     queryset = NotaModel.objects.all()
