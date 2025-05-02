22f8ec5d1e638c0bdd57b951f0483904a3978eb0
Edahi98 <garciaedahi3822@gmail.com>
2025-05-02 13:52:46 -0600

    se agrego jwt [Correacion]

--- backend/settings.py
+++ backend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'rest_framework.authtoken',
     'database_backend.apps.DatabaseBackendConfig',
 
