85ff8c8eee751326d12afaf5bf554212a3461d21
Edahi98 <garciaedahi3822@gmail.com>
2025-05-03 00:15:31 -0600

    se mejoro los resultados de la api de la bd

--- database_backend/views.py
+++ database_backend/views.py
@@ -4,19 +4,21 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from database_backend.models import ArticuloModel, TipoArticuloModel, NotaModel
 from database_backend.serializers import ArticuloSerializer, TipoArticuloSerializer, NotaSerializer
+from django.views.decorators.clickjacking import xframe_options_deny
 
-
+@xframe_options_deny
 class ArticuloViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
     serializer_class = ArticuloSerializer
-    queryset = ArticuloModel.objects.all()
+    queryset = ArticuloModel.objects.prefetch_related("notas").all()
 
+@xframe_options_deny
 class TipoArticuloViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
     serializer_class = TipoArticuloSerializer
-    queryset = TipoArticuloModel.objects.all()
+    queryset = TipoArticuloModel.objects.prefetch_related("articulos").all()
 
 class NotaViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
