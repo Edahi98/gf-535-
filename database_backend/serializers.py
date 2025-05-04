d9e2c453befcba5ab5f283b06ad7de86caf09ec5
Edahi98 <garciaedahi3822@gmail.com>
2025-05-04 01:14:04 -0600

    se corrigio problemas de los modelos y más [Correción]

--- database_backend/serializers.py
+++ database_backend/serializers.py
@@ -3,7 +3,6 @@ from .models import TipoArticuloModel, ArticuloModel, NotaModel
 from django.contrib.auth.models import User
 
 class NotaSerializer(ModelSerializer):
-    id_articulo = IntegerField(write_only=True)
     class Meta:
         model = NotaModel
         fields = [
@@ -15,7 +14,6 @@ class NotaSerializer(ModelSerializer):
 
 class ArticuloSerializer(ModelSerializer):
     notas = NotaSerializer(many=True, read_only=True)
-    id_articulo_tipo = IntegerField(write_only=True)
     class Meta:
         model = ArticuloModel
         fields = [
